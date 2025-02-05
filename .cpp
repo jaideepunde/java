f3f269cf6733a1b54830eec4f4c18871d7fe77b5
jaideep <jaideepunde78@gmail.com>
2025-02-05 14:49:52 -0800

    done

---
+++ .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[25];
+    cout<<"enter the size";
+    cin>> n;
+    cout<<"enter element of aray";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i]
+
+    }
+    cout<<"";
+    for (i=0.;i<n;i++)
+    {
+        cout<<a[i];
+    }
+     return 0 ;
+}
